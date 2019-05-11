553d25ebf1c4a871d199ea82ebe30643783cd7d8
Александр Чупиков <a.chupikov@movavi.com>
2019-05-11 17:19:51 +0700

    Add solution for task 2 of lesson 3

---
+++ lesson3/task2.hs
@@ -0,0 +1,12 @@
+-- Using a let expression and a lambda function aren’t exactly the same thing under
+-- the hood. For example, the following code will cause an error if you try to run it.
+--  Using a let expression and a lambda function aren’t exactly the same thing under
+-- the hood. For example, the following code will cause an error if you try to run it:
+
+counter x = let x = x + 1
+            in
+             let x = x + 1
+             in
+              x
+
+counterWithLambdas x = (\x -> (\x -> x) (x + 1)) (x + 1)
