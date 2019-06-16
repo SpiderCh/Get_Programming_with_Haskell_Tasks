701bbb9458c872cbe822a61cba1e303da87c5570
Alexander Chupikov <spider.ch@gmail.com>
2019-06-16 11:24:37 +0700

    Add solution for problem 2 of lesson 8

---
+++ lesson8/task2.hs
@@ -0,0 +1,26 @@
+-- Q8.2 Calculating Fibonacci numbers is perhaps the single
+-- most common example of a recursive function.
+-- The most straightforward definition is as follows:
+--  fib 0 = 0
+--  fib 1 = 1
+--  fib n = fib (n-1) + fib (n-2)
+-- Like the Ackermann function, this implementation quickly
+-- explodes due to the mutually recursive calls. But unlike
+-- the Ackermann function, there’s a much more efficient way to
+-- compute the nth Fibonacci number. Write a function, fastFib,
+-- that can compute the 1,000th Fibonacci number nearly instantly.
+-- Hint: fastFib takes three arguments: n1, n2, and counter.
+-- To calculate the 1,000th Fibonacci number, you call
+-- fastFib 1 1 1000 and for the 5th, you call fastFib 1 1 5.
+
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fastFib _ _ 0 = 0
+fastFib _ _ 1 = 1
+fastFib _ _ 2 = 1
+fastFib a b 3 = a + b
+fastFib a b count = fastFib newA a newCount
+    where newA = a + b
+          newCount = count - 1
